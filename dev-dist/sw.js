b65465317202d7da43c5f120292aa6e478176b09
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-06-04 18:43:13 +0200

    Revert "refactor: remove duplicate video files and update service worker cache revision"
    This reverts commit 47808b845ea39702c0d6e229321a1487333f4613.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.e3a8rebl3qo"
+    "revision": "0.ftagr4lpajg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
